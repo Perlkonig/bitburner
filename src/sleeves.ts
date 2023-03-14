0f06f2bfccf1c22da28e36cbbb438a41d5299c7f
Aaron Dalton <aaron@daltons.ca>
2023-03-14 15:13:59 -0600

    Sleeves can now study

--- src/sleeves.ts
+++ src/sleeves.ts
@@ -9,14 +9,24 @@ export async function main(ns: MyNS): Promise<void> {
     const karma = ns.heart.break();
 	const numSleeves = ns.sleeve.getNumSleeves();
 	for (let i=0; i<numSleeves; i++){
+        // First check for command-line argument
+        if (ns.args.length > 0) {
+            if (ns.args[0] === "study") {
+                ns.sleeve.setToUniversityCourse(i, "Rothman University", "Algorithms");
+                ns.tprint(`Setting sleeve #${i + 1} to study Algorithms.`);
+            }
         // If not yet at karma target, choose highest karmic return
-        if (Math.abs(karma) < karmaTarget) {
+        } else if (Math.abs(karma) < karmaTarget) {
             const crime = maxKarma(ns, ns.sleeve.getSleeve(i));
             ns.sleeve.setToCommitCrime(i, crime.type as CrimeType);
-            ns.tprint(`Setting sleeve #${i + 1} to crime ${crime.type}.`);
+            ns.tprint(`Setting sleeve #${i + 1} to crime ${crime.type} to maximize karma.`);
+        // Otherwise go for most money
         } else {
-            ns.sleeve.setToCommitCrime(i, "Homicide");
-            ns.tprint(`Setting sleeve #${i + 1} to default action of Homicide.`);
+            const crime = maxMoney(ns, ns.sleeve.getSleeve(i));
+            ns.sleeve.setToCommitCrime(i, crime.type as CrimeType);
+            ns.tprint(`Setting sleeve #${i + 1} to crime ${crime.type} to maximize money.`);
+            // ns.sleeve.setToCommitCrime(i, "Mug");
+            // ns.tprint(`Setting sleeve #${i + 1} to default action of Mug.`);
         }
 	}
 }
@@ -29,6 +39,27 @@ const maxKarma = (ns: NS, player: SleevePerson): CrimeStats => {
         const stats = ns.singularity.getCrimeStats(crime);
         const chance = ns.formulas.work.crimeSuccessChance(player, crime);
         const expected = stats.karma * chance / stats.time;
+        if (expected > bestReturn) {
+            bestReturn = expected;
+            bestCrime = stats;
+        }
+    }
+
+    if (bestCrime === undefined) {
+        throw new Error("There should always be at least one possible crime.");
+    }
+
+    return bestCrime;
+}
+
+const maxMoney = (ns: NS, player: SleevePerson): CrimeStats => {
+    let bestReturn = 0;
+    let bestCrime: CrimeStats | undefined = undefined;
+
+    for (const crime of crimes) {
+        const stats = ns.singularity.getCrimeStats(crime);
+        const chance = ns.formulas.work.crimeSuccessChance(player, crime);
+        const expected = stats.money * chance / stats.time;
         if (expected > bestReturn) {
             bestReturn = expected;
             bestCrime = stats;
