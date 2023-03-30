5cf81c1dd6b45e71afd94be6330bd7c21b93dc3e
Aaron Dalton <aaron@daltons.ca>
2023-03-30 10:21:06 -0600

    Completely redid the singularity script to account for BB

---
+++ src/player/bladeburner.ts
@@ -0,0 +1,174 @@
+import { BladeburnerCurAction, CityName, NS } from "@ns";
+
+const cities = ["Sector-12", "Chongqing", "New Tokyo", "Ishima", "Aevum", "Volhaven"];
+
+// const actionsGeneral = ["Training","Field Analysis","Recruitment","Diplomacy","Hyperbolic Regeneration Chamber","Incite Violence"];
+const actionsContracts = ["Tracking","Bounty Hunter","Retirement"].reverse();
+// Remove "Raid" to avoid the chaos hit
+const actionsOperations = ["Investigation","Undercover Operation","Sting Operation"/*,"Raid"*/,"Stealth Retirement Operation","Assassination"].reverse();
+const actionsBlackOps = ["Operation Typhoon","Operation Zero","Operation X","Operation Titan","Operation Ares","Operation Archangel","Operation Juggernaut","Operation Red Dragon","Operation K","Operation Deckard","Operation Tyrell","Operation Wallace","Operation Shoulder of Orion","Operation Hyron","Operation Morpheus","Operation Ion Storm","Operation Annihilus","Operation Ultron","Operation Centurion","Operation Vindictus","Operation Daedalus"];
+// const skills = ["Blade's Intuition","Cloak","Short-Circuit","Digital Observer","Tracer","Overclock","Reaper","Evasive System","Datamancer","Cyber's Edge","Hands of Midas","Hyperdrive"];
+
+const skillPriority = new Map<string, number>([
+    // Tracer is a 1 just because it should be levelled early,
+    // but it's capped at 10 levels
+    ["Tracer", 1],
+    ["Blade's Intuition", 1],
+    ["Digital Observer", 1],
+    ["Reaper", 1],
+    ["Evasive System", 1],
+    ["Cloak", 1.25],
+    ["Short-Circuit", 1.25],
+    ["Overclock", 1.25],
+    ["Hyperdrive", 2],
+]);
+
+const skillCaps = new Map<string, number>([
+    ["Cloak", 25],
+    ["Short-Circuit", 25],
+    ["Tracer", 10],
+    ["Hyperdrive", 20],
+]);
+
+const lowStaminaThresh = 0.5;
+const highStaminaThresh = 0.8;
+const lowHPThresh = 0; //0.2;
+const highHPThresh = 0.8;
+
+// /** @param {NS} ns **/
+// export async function main(ns: NS): Promise<void> {
+// }
+
+let msgs: string[] = [];
+let restTriggered = false;
+
+export const tick = (ns: NS): string => {
+    msgs = [];
+    allocateSkills(ns);
+    chooseCity(ns);
+
+    // Find out what the best option is
+    const best = chooseAction(ns);
+    if (best === undefined) {
+        msgs.push("Choosing to do nothing. Go work out or something!");
+    } else {
+        // Compare it to what we're doing and change if necessary
+        const curr = ns.bladeburner.getCurrentAction();
+        // ns.tprint(`Curr: ${JSON.stringify(curr)}, Best: ${JSON.stringify(best)}`);
+        if ( (curr.type === best.type) && (curr.name === best.name) ) {
+            // ns.print("Allowing the current action to continue.");
+            msgs.push("Allowing the current action to continue.");
+        } else {
+            const success = ns.bladeburner.startAction(best.type, best.name);
+            if (success) {
+                ns.print(`Changing action. Type: ${best.type}, Name: ${best.name}`);
+                msgs.push(`Changing action. Type: ${best.type}, Name: ${best.name}`);
+                if (best.type === "BlackOp") {
+                    ns.toast("Black Op in progress!", "warning", null);
+                }
+            } else {
+                ns.print(`ERROR Failed to change action. Type: ${best.type}, Name: ${best.name}`);
+                msgs.push(`ERROR Failed to change action. Type: ${best.type}, Name: ${best.name}`);
+            }
+        }
+    }
+
+    return `<p>${msgs.join("</p><p>")}</p>`;
+}
+
+const chooseCity = (ns: NS): void => {
+    const city = ns.bladeburner.getCity();
+    const population = ns.bladeburner.getCityEstimatedPopulation(city);
+    if (population <= 1e9) {
+        const pops = cities.filter(n => ns.bladeburner.getCityEstimatedPopulation(n as CityName) > 1e9);
+        if (pops.length > 0) {
+            const success = ns.bladeburner.switchCity(pops[0] as CityName);
+            if (success) {
+                ns.print(`Switching to the city ${pops[0]}.`);
+                msgs.push(`Switching to the city ${pops[0]}.`);
+                return;
+            } else {
+                ns.print(`ERROR Failed to move to the city ${pops[0]}.`);
+                msgs.push(`ERROR Failed to move to the city ${pops[0]}.`);
+            }
+        } else {
+            ns.toast("There are no cities with a population greater than 1 billion!", "warning", null);
+        }
+    }
+}
+
+const allocateSkills = (ns: NS): void => {
+    for (const [name, weight] of [...skillPriority.entries()].sort(() => Math.random() - Math.random())) {
+        const pts = ns.bladeburner.getSkillPoints();
+        const level = ns.bladeburner.getSkillLevel(name);
+        if ( (skillCaps.has(name)) && (level >= skillCaps.get(name)!) ) {
+            continue;
+        }
+        const cost = ns.bladeburner.getSkillUpgradeCost(name)
+        if (pts >= weight * cost) {
+            const success = ns.bladeburner.upgradeSkill(name);
+            if (success) {
+                ns.print(`Upgraded skill ${name} to level ${ns.bladeburner.getSkillLevel(name)}.`);
+                msgs.push(`Upgraded skill ${name} to level ${ns.bladeburner.getSkillLevel(name)}.`);
+            } else {
+                ns.print(`ERROR Something went wrong upgrading skill ${name}.`);
+                msgs.push(`ERROR Something went wrong upgrading skill ${name}.`);
+            }
+        }
+    }
+}
+
+const chooseAction = (ns: NS): BladeburnerCurAction|undefined => {
+    const [currStamina, maxStamina] = ns.bladeburner.getStamina();
+    const player = ns.getPlayer();
+
+    if ( (currStamina <= (maxStamina * lowStaminaThresh)) || ( (currStamina <= (maxStamina * highStaminaThresh)) && (restTriggered) ) || (player.hp.current <= (player.hp.max * lowHPThresh)) || ( (player.hp.current <= (player.hp.max * highHPThresh) ) && (restTriggered) ) ) {
+        restTriggered = true;
+        return {type: "General", name: "Hyperbolic Regeneration Chamber"};
+    } else {
+        restTriggered = false;
+        // Black Ops first
+        const blackOps = [...actionsBlackOps].filter(n => ns.bladeburner.getBlackOpRank(n) <= ns.bladeburner.getRank()).filter(n => ns.bladeburner.getActionCountRemaining("BlackOp", n) === 1).sort((a, b) => ns.bladeburner.getBlackOpRank(a) - ns.bladeburner.getBlackOpRank(b));
+        let blackOp: string|undefined = undefined;
+        if (blackOps.length > 0) {
+            blackOp = blackOps[0];
+        }
+        if (blackOp !== undefined) {
+            const [successMin, successMax] = ns.bladeburner.getActionEstimatedSuccessChance("BlackOp", blackOp);
+            const successAvg = (successMin + successMin + successMax) / 3;
+            if (successAvg > 0.85) {
+                return {type: "BlackOp", name: blackOp};
+            }
+        }
+
+        // Operations
+        for (const name of actionsOperations) {
+            const [successMin, successMax] = ns.bladeburner.getActionEstimatedSuccessChance("Operation", name);
+            const successAvg = (successMin + successMin + successMax) / 3;
+            const count = ns.bladeburner.getActionCountRemaining("Operation", name);
+            // const successes = ns.bladeburner.getActionSuccesses("Operation", name)
+            // const failures = ns.bladeburner.
+            if ( (successAvg>= 0.75) && (count > 0) ) {
+                return {type: "Operation", name};
+            }
+        }
+
+        // Contracts
+        for (const name of actionsContracts) {
+            const [successMin, successMax] = ns.bladeburner.getActionEstimatedSuccessChance("Contract", name);
+            const successAvg = (successMin + successMin + successMax) / 3;
+            const count = ns.bladeburner.getActionCountRemaining("Contract", name);
+            if ( (successAvg >= 0.60) && (count > 0) ) {
+                return {type: "Contract", name};
+            }
+        }
+
+        // If none of those are available, do something else minorly useful
+        const city = ns.bladeburner.getCity();
+        const chaos = ns.bladeburner.getCityChaos(city);
+        if (chaos > 45) {
+            return {type: "General", name: "Diplomacy"};
+        }
+    }
+    return {type: "General", name: "Field Analysis"};
+}
---
+++ src/player/manage.ts
@@ -0,0 +1,186 @@
+// 89.15GB
+
+import { CrimeType, Player } from "@ns";
+import { BoxNode, MyNS } from "../../MyTypes";
+import { createSidebarItem, sidebar } from "lib/box/box";
+import { maxKarma, maxStats } from "lib/crime";
+import { tick as bbTick } from "player/bladeburner";
+
+const studyUntilHackLevel = 50;
+
+// const megaCorps = ["Clarke Incorporated", "Bachman & Associates", "OmniTek Incorporated", "NWO", "Fulcrum Secret Technologies", "Blade Industries", "ECorp", "MegaCorp", "KuaiGong International", "Four Sigma"];
+
+const cityFactions = ["Sector-12", "Chongqing", "New Tokyo", "Ishima", "Aevum", "Volhaven"];
+
+// const crimes: CrimeType[] = ["Shoplift", "RobStore", "Mug", "Larceny", "Deal Drugs", "Bond Forgery", "Traffick Arms", "Homicide", "Grand Theft Auto", "Kidnap", "Assassination", "Heist"] as CrimeType[];
+
+// const ignoreFactionAugs = new Map([
+// 	["CyberSec", 'Cranial Signal Processors - Gen II'],
+// 	["NiteSec", 'DataJack'],
+// 	["The Black Hand", 'Embedded Netburner Module Core Implant'],
+// 	["Sector-12", 'Neuralstimulator'],
+// ])
+
+// interface IPurchaseOrder {
+//     augment: string;
+//     cost: number;
+//     faction: string;
+//     repNeeded: number;
+// }
+
+let globalStatus = "";
+let globalBBStatus = "";
+
+/** @param {MyNS} ns **/
+export async function main(ns: MyNS): Promise<void> {
+	ns.disableLog("ALL");
+    const box: BoxNode = createSidebarItem("Actions", "<p>Loading...</p>", "\ueb99") as BoxNode;
+
+    while (true) {
+        if ( (sidebar !== null) && (! sidebar.contains(box)) ) {
+            if (ns.bladeburner.inBladeburner()) {
+                ns.bladeburner.stopBladeburnerAction();
+            }
+            ns.exit();
+        }
+
+		const sleepTime = 5000;
+		const player = ns.getPlayer();
+
+        // programs
+        getPrograms(ns, player);
+
+        // home RAM
+        upgradeHomeServer(ns, player);
+
+        // always export when favour bonus
+        exportSave(ns);
+
+        /*
+         * Player needs a few things at start:
+         *   - Hacking level 50
+         *   - Stats for BB
+         *   - Karma for gang
+         *
+         * After that, let the bladeburner script manage activities
+         * until you have "The Blade's Simulacrum."
+         *
+         * Once you can double-act, do other stuff
+        */
+
+        const hasStats = (player.skills.strength >= 100) && (player.skills.defense >= 100) && (player.skills.agility >= 100) && (player.skills.dexterity >= 100);
+        const currWork = ns.singularity.getCurrentWork();
+        if (ns.getHackingLevel() < studyUntilHackLevel) {
+            if ( (currWork === null) || (! ("type" in currWork)) || (currWork.type !== "CLASS") ) {
+                ns.singularity.universityCourse("rothman university", "Study Computer Science", ns.singularity.isFocused());
+            }
+            globalStatus = `Studying to hacking level ${studyUntilHackLevel}.`
+        } else {
+            if ( (ns.getPlayer().bitNodeN !== 8) && ( (! ns.bladeburner.inBladeburner()) && (! hasStats) ) ) {
+                const crime = maxStats(ns, player);
+                if ( (currWork === null) || (! ("type" in currWork)) || (currWork["crimeType"] !== crime.type) ) {
+                    ns.singularity.commitCrime(crime.type as CrimeType, ns.singularity.isFocused());
+                }
+                globalStatus = `Committing the crime that produces the best stat gains.`;
+            } else {
+                if ( (ns.getPlayer().bitNodeN !== 8) && (! ns.bladeburner.inBladeburner()) && (hasStats) ) {
+                    ns.bladeburner.joinBladeburnerDivision();
+                    ns.toast("Joining Bladeburners!", "success", null);
+                } else if ( (! ns.bladeburner.inBladeburner()) && (! ns.gang.inGang()) && (ns.heart.break() > -54000) ) {
+                    const crime = maxKarma(ns, player);
+                    if ( (currWork === null) || (! ("type" in currWork)) || (currWork["crimeType"] !== crime.type) ) {
+                        ns.singularity.commitCrime(crime.type as CrimeType, ns.singularity.isFocused());
+                    }
+                    globalStatus = `Committing the crime that produces the fastest karma loss.`;
+                } else {
+                    const installed = ns.singularity.getOwnedAugmentations(false);
+                    if (installed.includes("The Blade's Simulacrum")) {
+                        globalStatus = "You can double act! Let's do something with it!";
+                    } else {
+                        globalStatus = "Letting the bladeburner script do its thing.";
+                        if (hasStats) {
+                            ns.singularity.stopAction();
+                        }
+                    }
+                }
+            }
+        }
+
+        if ( (ns.getPlayer().bitNodeN !== 8) && (ns.bladeburner.inBladeburner()) && (hasStats) && (ns.getHackingLevel() >= studyUntilHackLevel) ) {
+            globalBBStatus = bbTick(ns);
+        }
+
+        joinFactions(ns);
+
+        // gang
+        if ( (! ns.gang.inGang()) && (ns.heart.break() <= -54000) ) {
+            ns.gang.createGang("Slum Snakes");
+            ns.exec("gang.js", "home");
+            ns.toast("Gang created and management started.", "info", null);
+        }
+
+        // corp
+        if ( (! ns.corporation.hasCorporation()) && (ns.getServerMoneyAvailable("home") > 150e9) ) {
+            ns.toast("Seed funds available. Start corp!", "success", null);
+        }
+
+        renderBoxes(ns, box);
+		await ns.sleep(sleepTime);
+    }
+}
+
+const renderBoxes = (ns: MyNS, box:BoxNode): void => {
+    let bodyStr = `<p>${globalStatus}</p><hr><div>${globalBBStatus}</div>`;
+    if (ns.bladeburner.inBladeburner()) {
+        const [curr, max] = ns.bladeburner.getStamina();
+        const player = ns.getPlayer();
+        bodyStr += `<p>Stamina:&nbsp;<progress max="${max}" value="${curr}"></progress></p>`;
+        bodyStr += `<p>Health:&nbsp;<progress max="${player.hp.max}" value="${player.hp.current}"></progress></p>`;
+    }
+    box.body.innerHTML = bodyStr;
+    box.recalcHeight();
+}
+
+const exportSave = (ns: MyNS) => {
+    if (ns.singularity.exportGameBonus()) {
+        ns.singularity.exportGame();
+    }
+}
+
+const getPrograms = (ns: MyNS, player: Player) => {
+	if (player.money > 1700000) {
+		ns.singularity.purchaseTor();
+	}
+	else {
+		return;
+	}
+	ns.singularity.purchaseProgram("BruteSSH.exe");
+	ns.singularity.purchaseProgram("FTPCrack.exe");
+	ns.singularity.purchaseProgram("relaySMTP.exe");
+    ns.singularity.purchaseProgram("HTTPWorm.exe");
+    ns.singularity.purchaseProgram("SQLInject.exe");
+}
+
+function joinFactions(ns: MyNS) {
+    const newFactions = ns.singularity.checkFactionInvitations();
+    for (const faction of newFactions) {
+        if (!cityFactions.includes(faction)) {
+            ns.singularity.joinFaction(faction);
+            globalStatus = `Joined ${faction}`;
+        }
+    }
+}
+
+const upgradeHomeServer = (ns: MyNS, player: Player) => {
+	if (player.money > ns.singularity.getUpgradeHomeRamCost()) {
+		if (!player.factions.includes("CyberSec") || ns.singularity.getUpgradeHomeRamCost() < 2e9
+			|| ns.singularity.getUpgradeHomeRamCost() < 0.2 * player.money) {
+			const success = ns.singularity.upgradeHomeRam();
+			if (success) {
+				ns.print("Upgraded Home Server RAM");
+				ns.toast("Upgraded Home Server RAM");
+			}
+		}
+	}
+}
+
